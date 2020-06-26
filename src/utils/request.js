4ed379381d3f850a85c489365010901eede3dd29
Jin <512364579@qq.com>
2020-06-26 16:23:58 +0800

    feat: 修改token

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const isDevelopment = process.env.NODE_ENV == 'development'
 
-axios.defaults.headers.common['Authorization'] = "bearerey" + "JhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNjJkNjMyYy1kYmYyLTQyNWYtODVhNS05ZGQ1YmQzZWEwZWUiLCJ6b3dtMTIzIjoiRFlEUU9VIiwiZXhwIjoxNTk1NjQ5MzM0LCJpYXQiOjE1OTMwNTczMzR9.Mo9OqclnYRM2jCirrcpQdFWLghKxcWU1K4fpyuXFDRQk_p0IeJxgTXVl06XaMu09U01Agg5M1CAblUndtrxWLQ"
-
 // 请求拦截器
 axios.interceptors.request.use(    
     config => config,    
@@ -29,10 +27,16 @@ axios.interceptors.response.use(
 
 const httpAppInstance = axios.create({
     baseURL: isDevelopment ? '/h5Api' : 'http://app.zhongouwumeng.com',
+    headers: {
+        'Authorization': "bearerey" + "JhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNjJkNjMyYy1kYmYyLTQyNWYtODVhNS05ZGQ1YmQzZWEwZWUiLCJ6b3dtMTIzIjoiRFlEUU9VIiwiZXhwIjoxNTk1NjQ5MzM0LCJpYXQiOjE1OTMwNTczMzR9.Mo9OqclnYRM2jCirrcpQdFWLghKxcWU1K4fpyuXFDRQk_p0IeJxgTXVl06XaMu09U01Agg5M1CAblUndtrxWLQ"
+    },
 });
 
 const httpInstance = axios.create({
     baseURL: isDevelopment ? '/api' : 'http://47.114.81.48:8086',
+    headers: {
+        'Authorization': "bearerey" + "JhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjc1NjUyNTczNzQ0NzcxMDc0Iiwiem93bTEyMyI6IjFTV09QMiIsImV4cCI6MTU5NTc1MDgyOSwiaWF0IjoxNTkzMTU4ODI5fQ.oOMEGj9V4yKqxmbVWRubd5cr6d9AceJ3GU_N1ay4XoEqszWTXfvMXswsknQpdPzigvtk7NjUCJ7Lmu_q4PZ3_Q"
+    },
 });
 
 const createRequestInstanceFactory = (instance) => {
