3198a6e14e99c7afa5459225bcdc1e42fb1df51a
Jin <512364579@qq.com>
2020-08-01 21:55:13 +0800

    feat: 判断微信逻辑

--- src/views/SharePage/index.js
+++ src/views/SharePage/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect, PureComponent } from 'react';
+import { Modal } from 'antd-mobile'
 import { PageTitle, CButton, ShareTitle } from '../../components'
 import './index.scss';
 
-const SharePage = () => {
-    return (<div className="share-page">
-        <PageTitle title="下载分享" leftIcon="close"/>
-        <div className="share-page-content">
-            <ShareTitle />
-            <h4>中欧舞盟</h4>
-            <img className="share-page-content-img" src={require('../../assets/imgs/app-share.png')} alt=""/>
-            <CButton className="share-page-button-share-button">下载APP</CButton>
-        </div>
-    </div>)
+class SharePage extends PureComponent {
+
+    componentDidMount() {
+        if (this.wx()) {
+            Modal.alert('请点击右上角,通过浏览器下载')
+        }
+    }
+
+    wx = () => navigator.userAgent.toLowerCase().indexOf('micromessenger') !== -1
+
+    render() {
+        return (<div className="share-page">
+            <PageTitle title="下载分享" leftIcon="close"/>
+            <div className="share-page-content">
+                <ShareTitle />
+                <h4>中欧舞盟</h4>
+                <img className="share-page-content-img" src={require('../../assets/imgs/app-share.png')} alt=""/>
+                <CButton className="share-page-button-share-button">下载APP</CButton>
+            </div>
+        </div>)
+    }
 }
 
 export default SharePage
